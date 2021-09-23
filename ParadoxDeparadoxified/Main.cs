6ad6c5bcba8bcf85a57abd7520f34a461a677bb2
AbdoTGM50 <masterabdotgm50@ymail.com>
2021-09-23 19:32:36 +0200

    Initial Commit

---
+++ ParadoxDeparadoxified/Main.cs
@@ -0,0 +1,60 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.DialogSystem;
+using Kingmaker.DialogSystem.Blueprints;
+using UnityModManagerNet;
+
+namespace ParadoxDeparadoxified
+{
+    public class Main
+    {
+        public static bool Enabled;
+        public static UnityModManager.ModEntry mod;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            mod = modEntry;
+            modEntry.OnToggle = OnToggle;
+            return true;
+        }
+        
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Enabled = value;
+            return true;
+        }
+        
+        [HarmonyPatch(typeof(BlueprintsCache), "Init")]
+        static class BlueprintsCache_Init_Patch
+        {
+            static bool loaded = false;
+            
+            static void Postfix()
+            {
+                if (loaded) return;
+                loaded = true;
+                
+                PatchAnswer("9fc697452dfd3014b8282acf84973e4d"); // World
+                PatchAnswer("cde488feccbeb1b4094691e9bd607e9d"); // Arcana
+                PatchAnswer("4092d0d8beaff2c4891e15ef01e04723"); // Nature
+                PatchAnswer("d4f41f12693cd6d4b864ea1c39cdbb57"); // Religion
+                PatchAnswer("aa68a92b209268b498e249605c5ff2aa"); // Perception
+            }
+
+            static void PatchAnswer(string id)
+            {
+                var answer = ResourcesLibrary.TryGetBlueprint<BlueprintAnswer>(id);
+                if (answer != null)
+                {
+                    answer.CharacterSelection.SelectionType = CharacterSelection.Type.Clear;
+                    mod.Logger.Log($"Patched Answer: {id}");
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Paradox Deparadoxified
+
+Makes the Skill Checks needed for reassembling the "Lexicon of Paradox" party wide and doable by the highest available in the party instead of relying only on the main character's skill checks
+
+# How to install
+
+1. Download the latest zip file
+2. Install [UnityModManager](https://www.nexusmods.com/site/mods/21)
+3. Install the zip with UnityModManager
+
+# Credits
+
+- [Pathfinder Wrath of The Righteous Discord](https://discord.gg/bQVwsP7cky)
+- TylerGoeringer's [Modding Wiki](https://github.com/TylerGoeringer/OwlcatModdingWiki/wiki)
